05de439bc01fb698cbac39a9d738386c0872e6b4
IshaBahadoer <16680041-IshaBahadoer@users.noreply.replit.com>
2023-08-25 12:50:02 +0000

    Opdr 8

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
+var letterGrootte = 30;
+var marge = 20;
+
 function setup() {
-  canvas = createCanvas(900,200);
-  background('lavender');
+  canvas = createCanvas(1000,300);
   canvas.parent();
-  noLoop();
+  textFont("Courier");
+  textSize(letterGrootte);
+  textLeading(0.8*letterGrootte);
+  //noLoop();
 }
 
 function draw() {
-  noStroke();
-  fill('thistle');
+  background('orange');
+  fill('yellow');
+  textAlign(LEFT, TOP);
+  text("mouseX:" + round(mouseX) + " mouseY:" + round(mouseY),mouseX,mouseY);
   translate(50,50);
-  rect(0,0,100,100);
-  translate(140,0);
-  rect(0,0,100,100);
-  translate(140,0);
-  rect(0,0,100,100);
-  translate(140,0);
-  push();
-  translate(0,40);
-  stroke('cornsilk');
-  strokeWeight(20);
-  fill('teal');
-  rect(0,0,100,100);  
-  translate(140,0);
-  rect(0,0,100,100);
-  pop();  
-  translate(140,0);
-  rect(0,0,100,100);  
+  fill('black');
+  text("vensterGrootte:\n" + windowWidth + " x " + windowHeight,0,0);
+  text("canvasGrootte:\n " + width + " x " + height,0,120);
+  
+  translate(500,0);
+  fill('dodgerblue');
+  stroke('white');
+  strokeWeight(10);
+  rect(0,0,400,200);
+
+  translate(25,25);
+  noStroke();
+  fill('white');
+  textAlign(RIGHT, BOTTOM);
+  text("Deze tekst is erg lang en wordt daarom op meerdere regels weergegeven.",0,0,350,150);
 }
